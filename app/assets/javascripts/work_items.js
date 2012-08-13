af456ce1eb572110aa641a72fffe4fe931a8360c
Hajime Furukawa <hajpoj@gmail.com>
2012-08-12 21:33:45 -0700

    Finish ajaxifying the reports page:
     - Everything is based off of handle bar templates right now, instead of before,
     when half was done server side and half client side.
     - organized js code to be more structured and make more sense.

--- app/assets/javascripts/work_items.js
+++ app/assets/javascripts/work_items.js
@@ -5,72 +5,132 @@ $(function() {
   var $body = $('body');
 
   if($body.hasClass('reports show')) {
-    var $showForm = $('#new-work-item');
-    var $form = $('#new-work-item-form');
-    var $formSubmit = $form.find('.btn');
-    var $workItems = $('#work-items');
-    var editTemplate = Handlebars.compile($('#work-item-edit-template').html());
 
-    $showForm.on('click', function(e) {
-      e.preventDefault();
-      $form.slideToggle(100);
-    });
+    var workItemsController = {
 
-    $formSubmit.on('click', function(e) {
-      e.preventDefault();
-      var $formAction = $form.attr('action');
-      $.ajax({
-        type: 'POST',
-        url: $formAction,
-        data: $form.serialize(),
-        dataType: 'json',
-        success: function(data) {
-          console.log(data);
-          var source = $('#work-item-template').html();
-          var template = Handlebars.compile(source);
-          var html = template(data);
-          $workItems.append(html);
-        }
-      });
-    });
+      init: function() {
+        var self = this;
 
-    $workItems.on('click', '.edit.btn', function(e) {
-      e.preventDefault();
+        self.$workItemsContainer = $('#work-items-data');
+        self.initHandlebars();
+        self.initSubmitForm();
 
-      var $this = $(this);
-      var $workItemRow = $this.closest('.row');
-      var $description = $workItemRow.find('.desc.span10');
+        //display the current list of work items
+        self.initialDisplay();
 
-      var html = editTemplate({
-        description: $workItemRow.attr('data-desc'),
-        id: $workItemRow.attr('data-id'),
-        reportId: $workItemRow.attr('data-report-id')
-      });
-      $description.hide();
-      $workItemRow.prepend(html);
-    });
+        //Add event listener for edit work item button
+        self.$workItemsContainer.on('click', '.edit.btn', function(e) {
+          self.editWorkItem.call(self, e);
+        });
 
-    $workItems.on('click', '.update-item', function(e) {
-      e.preventDefault();
+        //Add event listener for updating work item button
+        self.$workItemsContainer.on('click', '.update-item', function(e) {
+          self.updateWorkItem.call(self, e);
+        });
+      },
 
-      var $updateForm = $(this).closest('form');
-      var $workItemRow = $(this).closest('.row');
-      var $updateAction = $updateForm.attr('action');
+      initHandlebars: function() {
+        var self = this;
+        self.workItemTemplate = Handlebars.compile($('#work-item-template').html());
+        self.editWorkItemTemplate = Handlebars.compile($('#edit-work-item-template').html());
 
-      $.ajax({
-        type: 'POST',
-        url: $updateAction,
-        data: $updateForm.serialize(),
-        dataType: 'json',
-        success: function(data) {
-          console.log(data);
-          var $description = $workItemRow.find('.desc.span10');
-          $description.html(data.description);
-          $description.show();
-          $updateForm.parent().remove();
-          $workItemRow.attr('data-desc', data.description);
-        }
-      });
-    });
+        Handlebars.registerHelper('json', function(context) {
+          return JSON.stringify(context);
+        });
+      },
+
+      initSubmitForm: function () {
+        var self = this;
+        self.$showFormButton = $('#new-work-item');
+        self.$form = $('#new-work-item-form');
+        self.$formSubmitButton = self.$form.find('.btn');
+
+        self.$showFormButton.on('click', function(e) {
+          e.preventDefault();
+          self.$form.slideToggle(100);
+        });
+
+        self.$formSubmitButton.on('click', function(e) {
+          self.addNewWorkItem.call(self, e);
+        });
+      },
+
+      initialDisplay: function() {
+        var self = this;
+        var data = self.$workItemsContainer.data('workitems');
+        var $data = $(data);
+
+        $data.each(function(index, workItem) {
+          var html = self.workItemTemplate(workItem);
+          self.$workItemsContainer.append(html);
+        });
+      },
+
+      editWorkItem: function(e) {
+
+        e.preventDefault();
+        var $target = $(e.target);
+        var self = this;
+        var $workItem = $target.closest('.work-item');
+        var $data = $workItem.data('item');
+        var html = self.editWorkItemTemplate($data);
+
+        $workItem.after(html);
+        $workItem.remove();
+
+        self.$workItemsContainer.find("option:[value="+$data.status+"]").attr('selected', 'true');
+      },
+
+      updateWorkItem: function(e) {
+        e.preventDefault();
+
+        var $target = $(e.target);
+        var self = this;
+        var $updateDiv = $target.closest('.edit-work-item');
+        var $updateForm = $target.closest('form');
+
+        $.ajax({
+          type: 'POST',
+          url:  $updateForm.attr('action'),
+          data: $updateForm.serialize(),
+          dataType: 'json',
+          success: function(data) {
+            self.updateWorkItemSuccess.call(self, data, $updateDiv);
+          }
+        });
+      },
+
+      updateWorkItemSuccess: function(data, $updateDiv) {
+        var self = this;
+        var html = self.workItemTemplate(data);
+        $updateDiv.after(html);
+        $updateDiv.remove();
+      },
+
+      addNewWorkItem: function(e) {
+        e.preventDefault();
+        var self = this;
+
+        $.ajax({
+          type: 'POST',
+          url: self.$form.attr('action'),
+          data: self.$form.serialize(),
+          dataType: 'json',
+          success: function(data) {
+            self.addNewWorkItemSuccess.call(self, data);
+          }
+        });
+      },
+
+      addNewWorkItemSuccess: function(data) {
+        var self = this;
+        var $textarea = self.$form.find('.description-field');
+        var html = self.workItemTemplate(data);
+        self.$workItemsContainer.append(html);
+
+        $textarea.focus().select();
+      }
+    };
+    workItemsController.init();
   }
 });
